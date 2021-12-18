a85b72f4fc7187f58b14287d4acd303d181be9eb
Steve Anderson <steven.a.anderson2012@gmail.com>
2021-12-18 10:11:30 -0700

    Add SGE Macros

---
+++ FFXIV/SGE.lua
@@ -0,0 +1,35 @@
+/macroicon "Diagnosis"
+/merror off
+/ac "Diagnosis" <mo>
+/ac "Diagnosis" <t>
+/ac "Diagnosis" <me>
+
+/macroicon "Druochole"
+/merror off
+/ac "Druochole" <mo>
+/ac "Druochole" <t>
+/ac "Druochole" <me>
+
+/macroicon "Taurochole"
+/merror off
+/ac "Taurochole" <mo>
+/ac "Taurochole" <t>
+/ac "Taurochole" <me>
+
+/macroicon "Icarus"
+/merror off
+/ac "Icarus" <mo>
+/ac "Icarus" <t>
+/ac "Icarus" <me>
+
+/macroicon "Haima"
+/merror off
+/ac "Haima" <mo>
+/ac "Haima" <t>
+/ac "Haima" <me>
+
+/macroicon "Kardia"
+/merror off
+/ac "Kardia" <mo>
+/ac "Kardia" <t>
+/ac "Kardia" <me>
