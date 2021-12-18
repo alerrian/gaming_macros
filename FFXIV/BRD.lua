d21ce15d7e9fb89a9b1669004efe19be879589b3
Steve Anderson <steven.a.anderson2012@gmail.com>
2021-12-18 10:17:21 -0700

    Add non healer macros

---
+++ FFXIV/BRD.lua
@@ -0,0 +1,5 @@
+/merror off
+/micon "Nature's Minne"
+/ac "Nature's Minne" <mo>
+/ac "Nature's Minne" <t>
+/ac "Nature's Minne" <me>
---
+++ FFXIV/WAR.lua
@@ -0,0 +1,5 @@
+/merror off
+/micon "Nascent Flash"
+/ac "Nascent Flash" <mo>
+/ac "Nascent Flash" <t>
+/ac "Nascent Flash" <me>
