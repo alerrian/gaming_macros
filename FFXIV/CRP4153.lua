551b559382ca75133f6d36646bcf0b3a515bbf31
Steve Anderson <steven.a.anderson2012@gmail.com>
2022-01-29 11:28:34 -0700

    Update Crafting macros

---
+++ FFXIV/CRP4153.lua
@@ -0,0 +1,15 @@
+/ac "Waste Not" <wait.2>
+/ac "Basic Touch" <wait.3>
+/ac "Standard Touch" <wait.3>
+/ac "Basic Touch" <wait.3>
+/ac "Standard Touch" <wait.3>
+/ac "Waste Not" <wait.2>
+/ac "Innovation" <wait.2>
+/ac "Basic Touch" <wait.3>
+/ac "Standard Touch" <wait.3>
+/ac "Basic Touch" <wait.3>
+/ac "Standard Touch" <wait.3>
+/ac "Veneration" <wait.2>
+/ac "Basic Synthesis" <wait.3>
+/ac "Basic Synthesis" <wait.3>
+/echo Craft finished <se.1>
